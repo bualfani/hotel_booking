bec07f2757a9e815adee1df595bf0ed110e5a159
bahati <bulozealfani@gmail.com>
2023-04-01 11:05:18 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+df = pd.read_csv("hotels.csv")
+
+class Hotel():
+    def book(self):
+        pass
+
+class Reservation():
+    def genrate(self):
+        pass
+
+id = input("Enter the id of the hotel: ")
